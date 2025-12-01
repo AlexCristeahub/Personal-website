764e4a1f7a19a5631b863dd23f26187e6f090854
AlexCristeahub <alexandrucristea246@gmail.com>
2025-12-01 11:38:49 +0200

    Add Google Tag Manager and Ahrefs analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W8MDB37K');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Ahrefs Analytics */}
+        <script src="https://analytics.ahrefs.com/analytics.js" data-key="mdMytTZ7ceLBwaAPJOyHVA" async />
+      </head>
       <body className={`${inter.variable} ${dancingScript.variable} font-sans antialiased bg-white text-black dark:text-white transition-colors duration-300`} style={{"--dark-bg": "#000000"} as React.CSSProperties}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-W8MDB37K"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <PHProvider>
           <ThemeProvider>
             <Suspense fallback={<div>Loading...</div>}>
